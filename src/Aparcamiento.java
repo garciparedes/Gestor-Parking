23317f4702a39cb69cdb93e51f36db657896e0a7
Sergio García <garciparedes@gmail.com>
2014-11-03 10:55:06 +0100

    Creadas las clases

---
+++ src/Aparcamiento.java
@@ -0,0 +1,5 @@
+/**
+ * Created by garciparedes on 3/11/14.
+ */
+public class Aparcamiento {
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by garciparedes on 3/11/14.
+ */
+public class Main {
+    /**
+     *
+     * @param Args
+     */
+    public static void main (String[] Args){
+
+    }
+}
