ef36363fa30da3a907b7756c37198fe9ffce050d
Adrian Calvo <adricalrojo@gmail.com>
2014-11-04 16:00:16 +0100

    Getters y Setters de Tarjeta

--- src/Aparcamiento.java
+++ src/Aparcamiento.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+
 /**
  * @author segarci, osferna, adrcalv
  */
 public class Aparcamiento {
+	
+	private ArrayList<Tarjeta> tarjetas;
+	
+	/**
+	 * 
+	 */
+	public boolean isDentro(String dni){
+		
+		Tarjeta tarjeta;
+		
+		for (int i = 0; i < tarjetas.size(); i++) {
+			
+			tarjeta = tarjetas.get(i);
+			
+			if(tarjeta.getDni().equals(dni) && !tarjeta.getFuera()){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public ArrayList<Tarjeta> isDentro(ArrayList<Tarjeta> tarjetas){
+			
+		
+		
+		return null;
+	}
 }
--- src/Tarjeta.java
+++ src/Tarjeta.java
@@ -1,10 +1,45 @@
+import java.util.GregorianCalendar;
+
 /**
  * @author segarci, osferna, adrcalv
  */
 public class Tarjeta {
 	
-	public Tarjeta() {
+	private GregorianCalendar fin_periodo = null;
+	private String nombre, apellido, dni;
+	private boolean fuera = true;
 
+	public Tarjeta(GregorianCalendar fin_periodo, String nombre, String apellido, String dni){
 		
+		this.fin_periodo = fin_periodo;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.setDni(dni);
+	}
+	
+	public static Tarjeta nueva(GregorianCalendar fin_periodo, String nombre, String apellido, String dni){
+		
+		return new Tarjeta(fin_periodo, nombre, apellido, dni);
+	}
+
+	public boolean getFuera() {
+		return fuera;
+	}
+
+	private void setFuera(boolean fuera) {
+		this.fuera = fuera;
+	}
+	
+	public void reset(){
+		
+		setFuera(true);
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
 	}
 }
